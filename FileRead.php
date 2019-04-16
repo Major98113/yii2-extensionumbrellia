690d8c7f7689aade75e6fab4c9f32c0911eaf914
max <meison98@bk.ru>
2019-04-16 17:57:55 +0300

    задокументировал код

--- FileRead.php
+++ FileRead.php
@@ -16,13 +16,12 @@ class FileRead extends \yii\base\Widget
     public function run()
     {
         if (file_exists($this->path)) { //проверяем, существует ли файл
-            $this->CheckDirectSize(filesize($this->path));
+            $this->CheckDirectSize(filesize($this->path));//если файл существует локально, смотрим его размер
         }
         else{ // если он находится удаленно, то file_exists выкинет false, поэтому делаю проверку на существование файла по url с помощью is_url_exist
 
             if($this->is_url_exist($this->path)){
-                $this->CheckDirectSize($this->curl_get_file_size($this->path));
-
+                $this->CheckDirectSize($this->curl_get_file_size($this->path));//если файл существует удаленно, проверяем его вес через curl
             }
             else{
                 return 'No such FILE : '.$this->path;
@@ -32,13 +31,13 @@ class FileRead extends \yii\base\Widget
 
 
 
-    public function searchLine($content)
+    public function searchLine($content) //функция для поиска слова в строке, выделил отдельно чтобы было удобнее ориентироваться
     {
         return stripos($content, $this->searchStr);
     }
 
 
-    public function is_url_exist($url){
+    public function is_url_exist($url){ //смотрим, есть ли файл удаленно
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_NOBODY, true);
         curl_exec($ch);
@@ -54,7 +53,7 @@ class FileRead extends \yii\base\Widget
         return $status;
     }
 
-    public function curl_get_file_size( $url ) {
+    public function curl_get_file_size( $url ) { //берем вес удаленного файла, если он существует
         $result = -1;
         $curl = curl_init( $url );
         curl_setopt( $curl, CURLOPT_NOBODY, true );
@@ -86,13 +85,12 @@ class FileRead extends \yii\base\Widget
     }
 
 
-    public function convertToBytes($from)
+    public function convertToBytes($from) //если задан параметр максимального веса файла, конвертируем это значение в байты
     {
         $units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'];
         $number = substr($from, 0, -2);
         $suffix = strtoupper(substr($from,-2));
 
-        //B or no suffix
         if(is_numeric(substr($suffix, 0, 1))) {
             return preg_replace('/[^\d]/', '', $from);
         }
@@ -106,10 +104,10 @@ class FileRead extends \yii\base\Widget
     }
 
 
-    public function CheckDirectSize($size){
+    public function CheckDirectSize($size){ //смотрим вес файла
         if (!empty($this->maxSize)){ //смотрим, задан ли третий параметр (maxSize)
             if ($this->convertToBytes($this->maxSize) >= $size ){ //если размер файла меньше или равен нашему maxSize, то идем дальше, в противном случае выкидываем ошибку
-                $this->GetSearchLines();
+                $this->GetSearchLines(); //смотрим файл для проверки наличия нужного слова
             }
             else{
                 echo "Too BIG SIZE";
@@ -117,7 +115,7 @@ class FileRead extends \yii\base\Widget
         }
         else{ //если maxSize не задан, начинаем поиск подстроки в файле
             if($size){
-                $this->GetSearchLines();
+                $this->GetSearchLines(); //смотрим файл для проверки наличия нужного слова
             }
         }
     }
@@ -128,10 +126,10 @@ class FileRead extends \yii\base\Widget
     public function GetSearchLines(){
         $fp = fopen($this->path, "r"); // Открываем файл в режиме чтения
         $counter = 0;
-        if ($fp)
+        if ($fp) //можно ли открыть файл?
         {
             $line = 0;
-            while (!feof($fp))
+            while (!feof($fp)) //проходим по всему файлу в поисках нужного слова в каждой строке
             {
                 $line++;
                 $mytext = fgets($fp, 999);
@@ -142,7 +140,7 @@ class FileRead extends \yii\base\Widget
                 }
             }
 
-            if ($counter==0){
+            if ($counter==0){ //если искомого слова не найдено в файле выводим сообщение
                 echo "Строка '$this->searchStr' не найдена в файле '$this->path'";
             }
         }
